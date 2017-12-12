c2fce8faa3f47d76f88f7286f2d078696fb77d5e
Shashank Singh <shashank9830@users.noreply.github.com>
2017-12-12 20:27:03 +0530

    Renamed hf-nester to pylist_nester

--- HeadFirstSeries/setup.py
+++ HeadFirstSeries/setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup (
     
-        name            = 'hf-nester',
+        name            = 'pylist_nester',
         version         = '1.0.0',
-        py_modules      = ['hf-nester'],
+        py_modules      = ['pylist_nester'],
         author          = 'shashank9830',
         author_email    = 'notrealemail@fakesite.com',
         url             = 'http://www.shashank9830.github.io/Python-HeadFirstSeries',
