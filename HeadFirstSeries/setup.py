8581d4a2797cf42272780204478aca4e02849dac
Shashank Singh <shashank9830@users.noreply.github.com>
2017-12-12 19:29:53 +0530

    Renaming package to work with an available version

--- HeadFirstSeries/setup.py
+++ HeadFirstSeries/setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup (
     
-        name            = 'nester',
+        name            = 'hf-nester',
         version         = '1.0.0',
-        py_modules      = ['nester'],
+        py_modules      = ['hf-nester'],
         author          = 'shashank9830',
         author_email    = 'notrealemail@fakesite.com',
         url             = 'http://www.shashank9830.github.io/Python-HeadFirstSeries',
