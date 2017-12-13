f4ede456f747e1a974bea81a380849ad92cf94e2
Shashank Singh <shashank9830@users.noreply.github.com>
2017-12-13 11:25:41 +0530

    v1.3.0 has option to turn indentation on and off

--- HeadFirstSeries/pylist_nester.py
+++ HeadFirstSeries/pylist_nester.py
@@ -2,16 +2,19 @@
     print_lol() which prints lists that may or may not include nested lists."""
 
 #Make level a default argument with init value 0
-def print_lol(the_list, level = 0):
+#Add indent variable to turn indentation on and off
+def print_lol(the_list, indent = False, level = 0):
 
     """This function takes a positional argument called "the_list", which is any
         Python list (of - possibly - nested lists). Each data item in the provided list
         is (recursively) printed to the screen on its own line.
-        A second argument called "level" is used to insert tab-stops when a nested list is encountered."""
+        The second argument "indent" can have True and False values which in turn switches
+        indentation on and off as per the choice of the user.
+        A third argument called "level" is used to insert tab-stops when a nested list is encountered."""
 
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level+1)
         else:
             #Adding indentations
             for tab_stop in range(level):
